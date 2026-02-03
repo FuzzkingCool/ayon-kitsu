3c3cf1c91a74d47c9ec5f0e1c0af98956ae15e43
Aleks Berland <aleksberland@gmail.com>
2026-02-03 13:54:31 -0500

    chore: Bump version to 1.2.6-dev.18 in version.py, package.py, and pyproject.toml

--- client/ayon_kitsu/version.py
+++ client/ayon_kitsu/version.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 """Package declaring kitsu addon version."""
-__version__ = "1.2.6-dev.17"
+__version__ = "1.2.6-dev.18"
--- package.py
+++ package.py
@@ -1,7 +1,7 @@
 
 name = "kitsu"
 title = "Kitsu"
-version = "1.2.6-dev.17"
+version = "1.2.6-dev.18"
 client_dir = "ayon_kitsu"
 
 services = {
