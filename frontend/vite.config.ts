96d5f1cbdb5676db54eead5a2d62a31f5a0bf57b
Aleks Berland <aleksberland@gmail.com>
2026-03-11 14:57:05 -0400

    fix: Update Vite configuration to use the correct directory for loading environment variables and set the project root

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,17 @@
+import { fileURLToPath } from "node:url";
+import path from "node:path";
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default ({ mode }) => {
-  Object.assign(process?.env, loadEnv(mode, process?.cwd(), ""));
+  Object.assign(process?.env, loadEnv(mode, __dirname, ""));
 
   const SERVER_URL = process?.env?.SERVER_URL || "http://localhost:5000";
 
   return defineConfig({
+    root: __dirname,
     server: {
       proxy: {
         "/api": {
