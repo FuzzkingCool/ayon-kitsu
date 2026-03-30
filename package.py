a0bc57b3ad4c572c1a0885f27b980cdefe9969d1
Aleks Berland <aleksberland@gmail.com>
2026-03-30 12:51:33 -0400

    chore: Bump version to 1.2.6+prod.0.1.4 in package.py and pyproject.toml files across main, processor, and tests directories

--- package.py
+++ package.py
@@ -1,12 +1,13 @@
 
 name = "kitsu"
 title = "Kitsu"
-version = "1.2.6-prod.0.1.0"
+version = "1.2.6+prod.0.1.4"
+_processor_image_tag = version.replace("+", "-")
 client_dir = "ayon_kitsu"
 
 services = {
     "processor": {
-        "image": f"ghcr.io/fuzzkingcool/ayon-kitsu-processor:{version}",
+        "image": f"ghcr.io/fuzzkingcool/ayon-kitsu-processor:{_processor_image_tag}",
         "environment": {
             "AYON_ADDON_NAME": name,
             "AYON_ADDON_VERSION": version,
